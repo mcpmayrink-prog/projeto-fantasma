fc4a4577b00903a1e9f2a448ed91c24b3d9a3561
mcpmayrink-prog <mcpmayrink@gmail.com>
2025-10-18 10:59:36 -0300

    Update entrega_2.R
    analise 2

--- rdocs/entrega_2.R
+++ rdocs/entrega_2.R
@@ -22,3 +22,26 @@ source("rdocs/source/packages.R")
 # de teste depreciados, ou ao menos deixando como comentário. Dê preferência
 # as funções dos pacotes contidos no Tidyverse para realizar suas análises.
 # ---------------------------------------------------------------------------- #
+
+# Transformando pounds em kilogramas
+
+relatorio$Weight_kls = relatorio$Weight_lbs * 0.453
+relatorio$Weight_lbs = NULL
+
+# Transformando dm em cm 
+relatorio$Height_cm = relatorio$Height_dm * 10
+relatorio$Height_dm = NULL
+
+
+
+library(tidyverse)
+
+
+
+graf = ggplot(relatorio) +
+  aes(x = Weight_kls, y = Height_cm) +
+  geom_point(colour = "#A11D21", size = 3) +
+  labs(x = "Peso do Cliente (quilogramas)",y = "Altura do Cliente (centímetros)") +
+  theme_estat()
+
+ggsave("disp_uni.pdf", width = 158, height = 93, units = "mm")
